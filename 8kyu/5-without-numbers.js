c6095f61b2f453f2c56d8c703b5a5bfc09288e71
Jason Huang <jason97521@gmail.com>
2023-02-02 07:10:59 -0500

    5 without number

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,16 @@
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+// Good luck :)
+
+//Parameters: No parameters
+//Results: Return a number value of 5
+//Examples:
+    //
+//Pseudocode: 
+    //1. Create a string with a length of five
+    //2. return the length of the string variable
+
+function unusualFive() {
+    let str = 'apple'
+    return str.length;
+}
