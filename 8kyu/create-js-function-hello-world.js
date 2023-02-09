738b0031c59403169af7a10e2e77e2740262f479
Jason Huang <jason97521@gmail.com>
2023-02-08 21:18:08 -0500

    create function hello world

---
+++ 8kyu/create-js-function-hello-world.js
@@ -0,0 +1,33 @@
+// Task
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+
+// type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+//Parameters: given no parameters
+//Results: Return a string value of 'Hello World!'
+//Examples:
+    // assert.isFunction(helloWorld, "function helloWorld is not defined")
+    // helloWorld();
+//Pseudocode: 
+    //1. Create a function called helloWorld
+    //2. Create a variable using let and set it equal to the string 'Hello World'
+    //3. Return the value of the string in a console log
+
+function helloWorld() {
+    let str = 'Hello World!'
+    return console.log(str)
+  }
