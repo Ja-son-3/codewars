3e513076ff824519d2f3b8f66a4a3203f47dc30a
Jason Huang <jason97521@gmail.com>
2023-02-22 16:58:30 -0500

    multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,22 @@
+// This code does not execute properly. Try to figure out why.
+
+// Given:
+    // function multiply(a, b){
+    //     a * b
+    // }
+  
+//Parameters: given two number parameters
+//Results: Return the product of the two parameters
+//Examples:
+    // assert.strictEqual(multiply(1,1), 1);
+    // assert.strictEqual(multiply(2,1), 2);
+    // assert.strictEqual(multiply(2,2), 4);
+    // assert.strictEqual(multiply(3,5), 15);   
+//Pseudocode: 
+    //1. Include a return statement to the given code, returning the product of parameter a and b.
+
+
+function multiply(a,b){
+    return a * b
+}
+  
