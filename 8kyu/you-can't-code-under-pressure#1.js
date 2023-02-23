321700018db6e2ab37a2b171dfa22c58c0d840a5
Jason Huang <jason97521@gmail.com>
2023-02-23 00:08:23 -0500

    you can't code under pressure#1

---
+++ 8kyu/you-can't-code-under-pressure#1.js
@@ -0,0 +1,13 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+//Parameters: Given an integer number parameter.
+//Results: Double the integer parameter and return it
+//Examples:
+    // Test.assertEquals(doubleInteger(2), 4);
+//Pseudocode: 
+    //1. multiply the given parameter by two and return it
+//Time Complexity: constant or 0(1)
+
+function doubleInteger(i) {
+    return i*2;
+}
