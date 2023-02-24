b5bf29e9a62303527f19ef94058aef124623a796
Jason Huang <jason97521@gmail.com>
2023-02-24 00:21:00 -0500

    function 1 hello world

---
+++ 8kyu/function-1-hello-world.js
@@ -0,0 +1,13 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+//Parameters: No parameter's
+//Results: Return a string of 'hello world!'
+//Examples:
+    // assert.strictEqual(greet(), "hello world!");
+//Pseudocode: 
+    //1. return a string of "hello world!"
+//Time Complexity: constant
+
+function greet() {
+    return "hello world!"
+}
