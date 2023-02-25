d9a332ca3082cd55338974ac4a7c60e9f48b380f
Jason Huang <jason97521@gmail.com>
2023-02-25 12:18:16 -0500

    returning strings

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,14 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//Parameters: Given a string parameter, representing a name
+//Results: Return a string saying Hello, <name> how are you doing today?, with <name> being the given name parameter
+//Examples:
+    // assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+    // assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+//Pseudocode: 
+    //1. Use a template literal with string interpolation to include the given name parameter in the return string.
+//Time Complexity: Constant or 0(1)
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
