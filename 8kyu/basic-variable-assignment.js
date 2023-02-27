c677d4b62cac114e0a774d71a72b648b9b9cc03c
Jason Huang <jason97521@gmail.com>
2023-02-27 05:42:27 -0500

    basic variable assignment

---
+++ 8kyu/basic-variable-assignment.js
@@ -0,0 +1,18 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//Given
+    // var a == "code";
+    // var b == "wa.rs";
+    // var name == a + b;
+
+//Parameters: Given two string parameters.
+//Results: Make name return a value of "codewa.rs"
+//Examples:
+    // assert.strictEqual(name, "codewa.rs");
+//Pseudocode: 
+    //1. Change the equal to operators to a declaration operator.
+//Time Complexity: Constant or 0(1)
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
