8a224b80c011c49fe900e3cb3dbfca60e7feba45
Jason Huang <jason97521@gmail.com>
2023-03-01 03:01:53 -0500

    function 2 squaring an argument

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,14 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//Parameters: Given a integer parameter
+//Results: Return the squared value of the given integer parameter.
+//Examples:
+    // assert.strictEqual(square(3), 9);
+//Pseudocode: 
+    //1. Create a function called square with a single parameter.
+    //2. Return a value of the given parameter multipled with itself.
+//Time Complexity: constant or 0(1)
+
+function square(n) {
+    return n * n
+}
