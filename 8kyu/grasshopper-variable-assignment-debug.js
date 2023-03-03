543bfa822cbc61d978ef5df5b0d72b7fd48003ef
Jason Huang <jason97521@gmail.com>
2023-03-03 07:23:25 -0500

    grasshopper variable assignment debug

---
+++ 8kyu/grasshopper-variable-assignment-debug.js
@@ -0,0 +1,28 @@
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//Parameters: No parameters.
+//Results: Return the concatenated value of a + b
+//Examples:
+//   it('should set variable a', () => {
+//     Test.assertEquals(a, 'dev')
+//   })
+//   it('should set variable b', () => {
+//     Test.assertEquals(b, 'Lab')
+//   })
+//   it('should set variable name', () => {
+//     Test.assertEquals(name, 'devLab')
+//Pseudocode: 
+    //1. Change the comparison operator to the set operator and return the results
+//Time Complexity: O(1) or Constant
+
+// Given
+// var a == "dev"
+// var b == "Lab"
+
+// var name == a + b
+
+//Solution
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
