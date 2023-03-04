2f8039e3247d5528f79289664b410c94d4a0e4cc
Jason Huang <jason97521@gmail.com>
2023-03-04 02:43:21 -0500

    grasshopper function syntax debugging

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,15 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them. 
+
+//Parameters: Given two string parameters, representing a verb and noun.
+//Results: Return a string of the verb plus the noun.
+//Examples:
+    // Test.assertEquals(main('take ', 'item'), 'take item')
+    // Test.assertEquals(main('use ', 'sword'), 'use sword')
+//Pseudocode: 
+    //1. Convert the square brackets containing the functions parameters into parenthesis.
+    //2. Encase the function in curly brackets.
+//Time Complexity: O(1) or Constant
+
+function main (verb, noun) {
+  return verb + noun
+}
