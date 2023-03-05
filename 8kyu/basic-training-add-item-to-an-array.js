79cef7c9e43b20e348f399b726bf390f9bba8a4e
Jason Huang <jason97521@gmail.com>
2023-03-05 02:29:41 -0500

    basic training add item to an array

---
+++ 8kyu/basic-training-add-item-to-an-array.js
@@ -0,0 +1,23 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+    // var websites = [];
+
+//Parameters: Given an array.
+//Results: Add the string of codewars to the given array.
+//Examples:
+    //   it("Should not be empty", () => {
+    //     assert.isNotEmpty(websites, 'The array is still empty')
+    //   })
+    //   it('Should contain "codewars"', () => {
+    //     assert.include(websites, 'codewars', 'The array does not contain the correct value "codewars"')
+    //   });
+    //   it("Should only contain 1 item", () => {
+    //     assert.lengthOf(websites, 1)
+    //   });
+//Pseudocode: 
+    //1. Use the push method to add the string of codewars into the websites array.
+//Time Complexity: Constant or O(1)
+
+websites.push("codewars")
